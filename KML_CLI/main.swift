93033423d03100801ee3b33f9d83e5b8cb5661e6
Matthew Elmore <elmo5676@gmail.com>
2018-08-02 07:34:44 -0700

    .

--- KML_CLI/main.swift
+++ KML_CLI/main.swift
@@ -9,33 +9,12 @@
 import Foundation
 
 
-
-//let orangePM = PlacemarkKML(placeMarkTitle: "New", placeMarkDescription: "", placeMarkCoords: [-122, 39]).iconGenerator(iconStyle: .orangeCircle, styleId: "new", withLabel: true)
-
-let coords: [Double] = [-122, 39]
-
-let alt = 500.0
-let coords01: [[Double]] = [[-120, 42, alt],[-119, 42, alt],[-119,41, alt],[-119, 42, alt]]
-
-let c2 = [[-118.85236, 35.032969999999999, alt], [-119.65495, 34.452820000000003, alt], [-117.76699000000001, 34.564369999999997, alt], [-117.39328999999999, 35.669649999999997, alt]]
-
-
-let magVar: Double = -13.0
-
-let u = Utilities()
+//Create KML below and call .writeToDocuments(fileName: <#T##String#>, ext: <#T##String#>) extension pn resulting String KML
 
 
 
 
 
-
-let newBE = BullsEyeKML(bullsEyeName: "TrythisShitOut",
-                        centerPoint: coords,
-                        radiusOfOuterRing: 150,
-                        magVariation: -13.0,
-                        lineColor: .red).BEKmlGenerator()
-
-let kml = OpenCloseKML(kmlItems: newBE).KMLGenerator().writeToDocuments(fileName: "SDF", ext: "kml")
 
 
 
